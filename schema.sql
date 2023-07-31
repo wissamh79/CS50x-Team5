92c9b9fd66129b6f09357b33802f70d104ad88e6
thamer-omar <thamerrap@gmail.com>
2023-07-31 18:06:18 +0300

    schema-update

--- schema.sql
+++ schema.sql
@@ -21,115 +21,51 @@ CREATE TABLE IF NOT EXISTS people (
 id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
 photo TEXT
+birthday date,
 );
 
-CREATE TABLE IF NOT EXISTS movies (
+CREATE TABLE IF NOT EXISTS songs (
 id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
+title TEXT NOT NULL, 
 release DATE,
 length TEXT,
 rating REAL,
 poster TEXT,
-trailer TEXT,
 is_featured BOOLEAN NOT NULL DEFAULT False
 );
 
-
-CREATE TABLE IF NOT EXISTS shows (
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-release DATE,
-rating REAL,
-poster TEXT,
-trailer TEXT,
-is_featured BOOLEAN NOT NULL DEFAULT False
-);
-
-CREATE TABLE IF NOT EXISTS seasons (
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-num_episodes INTEGER,
-release DATE,
-poster TEXT,
-trailer TEXT,
-show_id INTEGER,
-FOREIGN KEY (show_id) REFERENCES shows(id)
-);
-
-CREATE TABLE IF NOT EXISTS episodes (
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-release DATE,
-length TEXT,
-rating REAL,
-trailer TEXT,
-season_id INTEGER,
-FOREIGN KEY (season_id) REFERENCES seasons(id)
-ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS movies_actors (
+CREATE TABLE IF NOT EXISTS songs_singer (
 id INTEGER PRIMARY KEY,
 people_id INTEGER,
-movie_id INTEGER,
+songs_id INTEGER,
 FOREIGN KEY (people_id) REFERENCES people(id) ON DELETE CASCADE,
-FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+FOREIGN KEY (songs_id) REFERENCES songs(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS movies_directors (
+CREATE TABLE IF NOT EXISTS songs_directors (
 id INTEGER PRIMARY KEY,
 people_id INTEGER,
-movie_id INTEGER,
+songs_id INTEGER,
 FOREIGN KEY (people_id) REFERENCES people(id) ON DELETE CASCADE,
-FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+FOREIGN KEY (songs_id) REFERENCES songs(id) ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS movies_genres (
+CREATE TABLE IF NOT EXISTS songs_genres (
 id INTEGER PRIMARY KEY,
-genre_id INTEGER,
-movie_id INTEGER,
-FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE,
-FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+genres_id INTEGER,
+songs_id INTEGER,
+FOREIGN KEY (genres_id) REFERENCES genres(id) ON DELETE CASCADE,
+FOREIGN KEY (songs_id) REFERENCES songs(id) ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS shows_actors (
-id INTEGER PRIMARY KEY,
-people_id INTEGER,
-show_id INTEGER,
-FOREIGN KEY (people_id) REFERENCES people(id) ON DELETE CASCADE,
-FOREIGN KEY (show_id) REFERENCES shows(id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS shows_directors (
-id INTEGER PRIMARY KEY,
-people_id INTEGER,
-show_id INTEGER,
-FOREIGN KEY (people_id) REFERENCES people(id) ON DELETE CASCADE,
-FOREIGN KEY (show_id) REFERENCES shows(id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS shows_genres (
-id INTEGER PRIMARY KEY,
-genre_id INTEGER,
-show_id INTEGER,
-FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE,
-FOREIGN KEY (show_id) REFERENCES shows(id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS users_movies (
+CREATE TABLE IF NOT EXISTS users_songs (
 id INTEGER PRIMARY KEY,
 user_id INTEGER,
-movie_id INTEGER,
+songs_id INTEGER,
 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+FOREIGN KEY (songs_id) REFERENCES songs(id) ON DELETE CASCADE
 );
 
 
